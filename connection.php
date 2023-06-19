90ee0b3569fcc3b1c8bae9f49eb0549c13707b92
kienltgcc200184 <kienltgcc200184@fpt.edu.vn>
2023-06-19 17:48:27 +0700

    4

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     // $conn = mysqli_connect('localhost','root','','atndb') 
     // or die("Cannot connect database".mysqli_connect_error());
 
-    $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","	b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
+    $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
     or die("Cannot connect database".mysqli_connect_error());
 
     mysqli_query($conn, 'SET NAMES "utf8"')
