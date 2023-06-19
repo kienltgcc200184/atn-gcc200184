80b294c26468dba78424725d1c1af973e15e8be9
kienltgcc200184 <kienltgcc200184@fpt.edu.vn>
2023-06-19 17:43:12 +0700

    3

--- connection.php
+++ connection.php
@@ -5,5 +5,5 @@
     $conn = mysqli_connect("nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","im6uv6tb0uzh0kef","	b4ihu2lkxh8ho1b2","mhzmhjrrq860l1d5") 
     or die("Cannot connect database".mysqli_connect_error());
 
-    // mysqli_query($conn, 'SET NAMES "utf8"')
+    mysqli_query($conn, 'SET NAMES "utf8"')
 ?>
